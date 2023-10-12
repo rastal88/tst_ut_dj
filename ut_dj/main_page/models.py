58066d371011cb60bdad42f16edbf5697b330314
Stas Rac <ra.st.al88z65@gmail.com>
2023-10-12 22:58:14 +0300

    add MenuItem model

--- ut_dj/main_page/models.py
+++ ut_dj/main_page/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    url = models.CharField(max_length=100, null=True, blank=True)
+    named_url = models.CharField(max_length=100, null=True, blank=True)
+
+    def get_absolute_url(self):
+        if self.url:
+            return self.url
+        elif self.named_url:
+            return reverse(self.named_url)
+        return "#"
+
+    def __str__(self):
+        return self.title
+
