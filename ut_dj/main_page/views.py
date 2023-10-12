057dc5f9c836bb947cbd84d05ddef6b9c9f8a148
Stas Rac <ra.st.al88z65@gmail.com>
2023-10-12 23:01:20 +0300

    add view menu_item_detail

--- ut_dj/main_page/views.py
+++ ut_dj/main_page/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from main_page.models import MenuItem
+class MainView(TemplateView):
+    template_name = 'index.html'
 
-# Create your views here.
+def menu_item_detail(request, menu_item_id):
+    menu_item = get_object_or_404(MenuItem, pk=menu_item_id)
+    return render(request, 'item_detail.html', {'menu_item': menu_item})
